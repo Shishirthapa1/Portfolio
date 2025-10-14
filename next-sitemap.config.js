449250d3427e4ee0f6b7a7783d3ef840e6501963
Shishirthapa1 <sisirjangthapa@gmail.com>
2025-10-14 22:31:07 +0545

    changes for seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,20 @@ module.exports = {
   siteUrl: "https://shishirthapa.vercel.app", // your live site URL
   generateRobotsTxt: true, // generates robots.txt as well
   sitemapSize: 7000,
-  changefreq: "weekly",
+  changefreq: "daily",
   priority: 0.7,
+  // ✅ for large sites
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+    ],
+    additionalSitemaps: ["https://shishirthapa.vercel.app/sitemap.xml"],
+  },
 };
