8af9123abcfe7e7bf4038d5572ed89cc6b8410a0
Shishirthapa1 <sisirjangthapa@gmail.com>
2025-10-20 12:08:53 +0545

    changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,15 +2,16 @@
 module.exports = {
   siteUrl: "https://shishirthapa.vercel.app",
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
   sitemapSize: 7000,
   changefreq: "daily",
   priority: 0.7,
-  generateIndexSitemap: false, // ❌ key change: generate only one sitemap
   robotsTxtOptions: {
     policies: [
-      { userAgent: "*", allow: "/" },
-      // { userAgent: "Googlebot", allow: "/" },
+      {
+        userAgent: "*",
+        allow: "/",
+      },
     ],
-    additionalSitemaps: [], // no need to add sitemap.xml itself here
   },
 };
